410d37283d637c96655dcb52419b3f22b8c8ac63
fortunate-jest-6040 <pedro.loureiro7@hotmail.com>
2019-05-06 23:09:02 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,14 @@
 def unsafe?(speed)
   if speed < 40 || speed > 60
-    true
+    return true
   else
-    false
+    return false
 end
 
 
 
 def not_safe?(speed)
-	speed < 40 |||speed > 60 ? true : false
+	speed < 40 |||speed > 60 ? return true : return false
 end
 	
 
